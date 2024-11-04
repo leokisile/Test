365be1aeec5446226d1c1f8406420541157615bd
leokisile <leokisile1309@gmail.com>
2024-11-04 14:04:37 -0600

    Version 1

---
+++ strings.py
@@ -0,0 +1,9 @@
+nombre = " ada lovelace "
+
+print(nombre.title()) #Da formato de mayusculs despues de cada espacio
+print(nombre.upper()) #Pone todo en mayusculas
+print(nombre.lower()) #Pone todo en minusculas
+
+print(nombre.rstrip())#Elimina el espacio a la darecha
+print(nombre.lstrip())#Elimina el espacio a la izquierda
+print(nombre.strip())#Elimina ambos espacios
